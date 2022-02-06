e19c382d7c8cc150adaf2fed1e793436ab11e367
Tyler Santos <tylersantostj@gmail.com>
2022-02-06 02:04:15 -0800

    Initial Commit

---
+++ app/src/main/java/edu/up/cs301customcoloring_minecraft/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.up.cs301customcoloring_minecraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
